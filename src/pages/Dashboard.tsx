dce09ffd5fb806daf5b6788810bdcc4434b0cbeb
rehahlas12-afk <rehahlas12@gmail.com>
2026-05-06 17:34:51 +0200

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -19,8 +19,8 @@ const Dashboard = () => {
 
   const handleQuit = async () => {
     const ok = window.confirm("Voulez-vous quitter l'application ?");
-    if (!ok) Application.close();
-
+    if (!ok)finishAffinity ();
+finis
   
   };
 
