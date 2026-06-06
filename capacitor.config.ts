a56aa3aae1b4b0ab17fc973c84f5a2fd53ff4f7b
rehahlas12-afk <rehahlas12@gmail.com>
2026-06-06 21:17:45 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: "com.monapp.joy",
   appName: "code-to-apk-joy",
   webDir: "dist",
-  server: {
-    url: "https://23dcaa64-4079-4653-945f-bb1961e32b87.lovableproject.com?forceHideBadge=true",
-    cleartext: true,
-  },
+  
+    
+    
+  
 };
 
 export default config;
