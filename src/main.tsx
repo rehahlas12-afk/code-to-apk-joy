3affcf47f416a34a50685b86d34e96f8c53f755f
rehahlas12-afk <rehahlas12@gmail.com>
2026-06-07 00:27:41 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import App from "./App.tsx";
 import "./index.css";
+import { Clipboard } from '@capacitor/clipboard';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Demande l'autorisation du micro au démarrage de l'application
+navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(() => console.log("Micro autorisé"))
+  .catch((err) => console.log("Erreur micro:", err));
+
+createRoot(document.getElementById('root')!).render(<App />);
+
