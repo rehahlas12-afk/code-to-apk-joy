1dafe9fa82e72f1e804608bc443e9fef7f9527cc
rehahlas12-afk <rehahlas12@gmail.com>
2026-06-07 07:24:43 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+
+  import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.monapp.joy",
-  appName: "Sabrinos",
-  webDir: "dist",
+  appId: 'com.sabrinis.app',
+  appName: 'Sabrinos',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
   plugins: {
     Permissions: {}
   }
 };
 
 export default config;
-  
 
